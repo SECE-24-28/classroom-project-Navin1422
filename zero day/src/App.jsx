06f8f05796634cc7bf077b1681a18d95845af885
Navin1422 <navinzoro7@gmail.com>
2025-12-03 08:55:13 +0530

    DAY 2 WORK

---
+++ zero day/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import FlexBoxComponent from "./flex-box/flex-box-component";
+import HelloWorldComponent from "./Hello-world/Hello_World_Component";
+import StudentForm from "./StudentRegistrationForm/StudentRegistrationForm.jsx"
+
+function App() {
+  return (
+    <div> 
+      <HelloWorldComponent />
+    </div>
+  );
+}
+
+export default App
